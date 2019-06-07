1f275b8c83c0612b49142e5cce0b535ad9c86cf9
Beato Bongco <beatobongco@gmail.com>
2019-06-07 17:02:34 +0800

    Add setup and __init__.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bertsum",
+    version="0.0.1",
+    author="@nlpyang (packaged by @beatobongco)",
+    description="BERT for text summarization",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nlpyang/BertSum",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Apache License 2.0",
+    ],
+)
