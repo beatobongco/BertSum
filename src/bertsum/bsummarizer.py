ef65d05f47c939f203762c787a564f1d4f7c27e4
Beato Bongco <beatobongco@gmail.com>
2019-06-09 22:21:30 +0800

    Handle not enough text error, make code more efficient

--- src/bertsum/bsummarizer.py
+++ src/bertsum/bsummarizer.py
@@ -82,6 +82,7 @@ class BSummarizer(object):
             "vocab_size": 30522
         })
 
+        self.BertData = data_builder.BertData(self.pp_args)
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         model = Summarizer(self.args, self.device, load_pretrained_bert=False, bert_config=self.bert_config)
         model.load_cp(torch.load(model_path, map_location=lambda storage, loc: storage))
@@ -108,58 +109,44 @@ class BSummarizer(object):
         Summarizes input text by returning the most important sentences based on the BERT model fine-tuned on CNN and Daily Mail articles
         """
         # Separate documents into list of sentences
-
         src = [sent.split() for sent in nltk.tokenize.sent_tokenize(src_str)]
-        tgt = [sent.split() for sent in nltk.tokenize.sent_tokenize(tgt_str)]
-        bert = data_builder.BertData(self.pp_args)
-        oracle_ids = data_builder.greedy_selection(src, tgt, 3)
-        b_data = bert.preprocess(src, tgt, oracle_ids)
+        b_data = self.BertData.preprocess(src, [], [])
+
+        if not b_data:
+            return {"error": "Not enough text to create a summary."}
+
         indexed_tokens, labels, segments_ids, cls_ids, src_txt, tgt_txt = b_data
-        b_dict = {
-            "src": indexed_tokens,
-            "labels": labels,
-            "segs": segments_ids,
-            "clss": cls_ids,
-            "src_str": src_txt,
-            "tgt_str": tgt_txt,
-        }
         data = [
             [
-                b_dict["src"],
-                b_dict["labels"],
-                b_dict["segs"],
-                b_dict["clss"],
-                b_dict["src_str"],
-                b_dict["tgt_str"],
+                indexed_tokens,
+                labels,
+                segments_ids,
+                cls_ids,
+                src_txt,
+                tgt_txt,
             ]
         ]
         batch = data_loader.Batch(data, is_test=True, device=self.device)
-
         sent_scores, mask = self.model(batch.src, batch.segs, batch.clss, batch.mask, batch.mask_cls)
         sent_scores = sent_scores + mask.float()
         sent_scores = sent_scores.cpu().data.numpy()
         # Sort sentence ids in descending order based on sentence scores (representing summary importance)
-        selected_ids = np.argsort(-sent_scores, 1)
-        for i in selected_ids:
-            _pred = []
-            if len(batch.src_str[i]) == 0:
-                continue
-            # Loop through each sentence
-            # len(batch.src_str[i]) refers to the number of sentences in the jth test batch
-            for j in selected_ids[i][: len(batch.src_str[i])]:
-                if j >= len(batch.src_str[i]):
-                    continue
-                candidate = batch.src_str[i][j].strip()
-                if self.args.block_trigram:
-                    if not self._block_tri(candidate, _pred):
-                        _pred.append(candidate)
-                else:
+        # BRB: Since batch size is just 1 we can take the first element of the scored sentences and source string
+        scored_sentences = np.argsort(-sent_scores, 1)[0]
+        article = batch.src_str[0]
+        _pred = []
+
+        # Loop through each sentence
+        for i in scored_sentences:
+            candidate = article[i].strip()
+            if self.args.block_trigram:
+                if not self._block_tri(candidate, _pred):
                     _pred.append(candidate)
+            else:
+                _pred.append(candidate)
 
-                # len(_pred) == 3 means that we limit sentences to top top_n_sentences
-                if len(_pred) == top_n_sentences:
-                    break
+            # len(_pred) == 3 means that we limit sentences to top top_n_sentences
+            if len(_pred) == top_n_sentences:
+                break
 
-            _pred = " ".join(_pred)
-
-        return _pred
+        return {"result": " ".join(_pred)}
